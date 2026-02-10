c03219f7a720c5ee87d9595f181c916dd3798745
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-10 19:04:20 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -42,8 +42,8 @@ st.markdown(f"""
         /* --- THE LOGO FIX: UPPER LEFT CORNER --- */
         .top-left-logo {{
             position: fixed;
-            left: 10px;
-            top: 10px;
+            left: 30px;
+            top: 5px;
             width: 130px; /* Adjust size as needed */
             z-index: 10001;
         }}
