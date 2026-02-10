8bc9fca200c922d3a299a82c111d80b128aea1f3
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-10 14:29:53 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -325,7 +325,7 @@ with st.container(border=True):
 # Footer
 st.markdown("""
     <div class="footer">
+        <p style="font-size: 15px; margin-top: 5px;">Sponsored By <b class="brand-text">Uday Mondal</b> | Consultant Advocate</p>
         <p>Powered & Created by <b class="brand-text">Debasish Biswas</b> | Professional Tally Automation</p>
-        <p style="font-size: 14px; margin-top: 5px;">Sponsored By <b class="brand-text">Uday Mondal</b> | Consultant Advocate</p>
     </div>
 """, unsafe_allow_html=True)
